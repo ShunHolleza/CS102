50fa23640eab8b1141bd39e87c3c7f3c76662e63
ShunHolleza <shun.holleza@students.isatu.edu.ph>
2024-03-18 06:06:21 +0000

    Activity1

---
+++ ACTIVITY#1/3rdItem.R
@@ -0,0 +1,59 @@
+library(rvest)
+library(polite)
+
+URL <- "https://www.amazon.com/s?k=basketball+shoes+for+men+nike&crid=3N1HEAQVRVNE7&sprefix=basketball+shoes+for+men+%2Caps%2C482&ref=nb_sb_ss_ts-doa-p_1_25"
+
+session <- bow(url,
+               user_agent = "Educational")
+session
+
+sessionPage <- scrape(session)
+
+divElements <- html_nodes(sessionPage, 'div.sg-col-4-of-24 sg-col-4-of-12 s-result-item s-asin sg-col-4-of-16 sg-col s-widget-spacing-small sg-col-4-of-20')
+
+brandDes <- character()
+prices <- character()
+revCounts <- character()
+ratings <- character()
+
+productDount <- 0
+
+for (divElement in divElements) {
+  brandElement <- html_node(divElement, 'span.a-size-base-plus.a-color-secondary.a-text-normal')
+  brand <- ifelse(!is.na(brandElement), html_text(brandElement), '')
+  
+  desElement <- html_node(divElement, 'div.a-section.a-spacing-small')
+  description <- ifelse(!is.na(desElement), html_text(desElement), '')
+  
+  priceElement <- html_node(divElement, 'span.a-price-whole')
+  price <- ifelse(!is.na(priceElement), html_text(priceElement), '')
+  
+  revCountElement <- html_node(divElement, 'span.a-size-base')
+  revCount <- ifelse(!is.na(revCountElement), html_text(revCountElement), '')
+  
+  ratingElement <- html_node(divElement, 'span.a-icon-alt')
+  rating <- ifelse(!is.na(ratingElement), html_text(ratingElement), '')
+  
+  brandDes <- paste(brand, description)
+  brandDescriptions <- c(brandDescriptions, brandDes)
+  prices <- c(prices, price)
+  revCounts <- c(revCounts, revCount)
+  ratings <- c(ratings, rating)
+  
+  
+}
+
+DF <- data.frame(Brand_Description = brand_descriptions,
+                 Price = prices,
+                 Review_Count = review_counts,
+                 Rating = ratings)
+
+write.csv(DF, "3rdproduct.csv")
+
+DF$Category <- "BasketballShoes"
+
+DF <- DF[, c("Category", names(DF)[-which(names(DF) == "Category")])]
+
+write.csv(DF, "3rdproduct2.csv", row.names = FALSE)
+
+View(SecondProduct)
